260a32c04b472aaeb0c17d12f7a1aadf40c9e2e5
Wiktor1666 <wiktorkazmierczak6@gmail.com>
2025-11-12 14:13:18 +0100

    Zadania z zajęć

--- lab5/lab5.py
+++ lab5/lab5.py
@@ -11,16 +11,38 @@ def zad1a():
     są liczbami nieparzystymi
     """
     n = abs(int(input("Podaj n: ")))
+    count = 0
+    for i in range(n):
+        num = int(input("Podaj liczbe: "))
+        if num % 2 == 1:
+            count += 1
+    print("Ilość liczb nieparzystych: ", count)
 
 def zad1b():
     """
     są podzielne przez 3 i niepodzielne przez 5
     """
+    n = int(input("Podaj n: "))
+    count = 0
+    for i in range(n):
+        num = int(input("Podaj liczbe: "))
+        if num % 3 == 0 and num % 5 != 0:
+            count += 1
+    print("Ilość liczb podzielnych przez 3 i niepodzielnych przez 5: ", count)
 
 def zad1c():
     """
     są kwadratami liczby parzystej
     """
+    import math
+    n = int(input("Podaj n: "))
+    count = 0
+    for i in range(n):
+        num = int(input("Podaj liczbe: "))
+        root = int(math.sqrt(num))
+        if root * root == num and root % 2 == 0:
+            count += 1
+    print("Ilość kwadratów liczb parzystych: ", count)
 
 def zad1d():
     """
@@ -46,6 +68,19 @@ def zad1e():
     """
     spełniają warunek 2^k <a_k < k! dla 1 <= k <= n
     """
+    import math
+    n = int(input("Podaj n: "))
+    a = []
+    for i in range(n):
+        a.append(int(input("Podaj liczbe: ")))
+    count = 0
+    for k in range(1, n):
+        left = 2 ** k
+        right = math.factorial(k)
+        if left < a[k] < right:
+            count += 1
+
+    print("Ilość liczb spełniających warunek: ", count)
 
 def zad1f():
     """
